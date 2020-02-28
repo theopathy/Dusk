cc9b9015c63fcc0e23442c7f85fe69538a754c24
Theopathy <EvanWrynn@gmail.com>
2020-02-28 22:36:42 +0000

    Docs Starting
    Started up docs

--- projects/Ikimono/Ikimono.runtime.js
+++ projects/Ikimono/Ikimono.runtime.js
@@ -9,9 +9,23 @@ console.log('%cIkimono: Counting Creatures...', 'background: #fba; color: #342')
 var Httpreq = new XMLHttpRequest();
 Httpreq.open("GET",_DEBUG ? "creatures/fake-compile.txt" : "creatures/compile.php" , false);
 Httpreq.send(null);
-Ikimono.creatures = JSON.parse(Httpreq.responseText);
-Ikimono.countCreatures = Ikimono.creatures.length;
+Ikimono.creaturesJSONPATH = JSON.parse(Httpreq.responseText);
+Ikimono.countCreaturesj = Ikimono.creaturesJSONPATH.length;
+Ikimono.creatures = [];
+Ikimono.countCreatures = 0;
+Ikimono.creaturesJSONPATH.forEach(function (creat) {
 
-    console.log(Httpreq.responseText);
-console.log('%cIkimono: Registered Creatures: %s ', 'background: #fba; color: #342',Ikimono.countCreatures);
+   fetch("creatures/"+creat)
+   .then(res => {return res.text();})
+  .then(data => {   var imonos = JSON5.parse(data);
+    Ikimono.creatures[imonos.Name] = imonos;  
+    Ikimono.countCreatures++;
+    if (Ikimono.countCreatures == Ikimono.countCreaturesj) _goto(); 
+}); 
 
+})
+
+function _goto() {
+    console.log('%cIkimono: Registered Creatures: %s ', 'background: #fba; color: #342',Ikimono.countCreatures);
+
+}
--- projects/Ikimono/creatures/compile.php
+++ projects/Ikimono/creatures/compile.php
@@ -1,8 +1,8 @@
 <?php 
 $files = array();
-foreach (glob("*.json") as $file) {
+foreach (glob("*.json5") as $file) {
   $files[] = $file;
 }
 
 echo json_encode($files);
-file_put_contents("fake-compile.txt",json_encode($files));
+file_put_contents("fake-compile.txt",json_encode($files)); 
